7718b44cf96a052e0e30ce9f31a48ffc18c668a4
fang-jingwu <2970617302@qq.com>
2024-05-16 22:37:39 +0800

    在系统管理菜单下添加了活动列表子菜单，用于查看，修改，删除活动

---
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/domain/SysActivity.java
@@ -0,0 +1,79 @@
+/*
+*  Copyright 2019-2020 Zheng Jie
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+package me.zhengjie.modules.system.domain;
+
+import lombok.Data;
+import cn.hutool.core.bean.BeanUtil;
+import io.swagger.annotations.ApiModelProperty;
+import cn.hutool.core.bean.copier.CopyOptions;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import org.hibernate.annotations.*;
+import java.sql.Timestamp;
+import java.io.Serializable;
+
+/**
+* @website https://eladmin.vip
+* @description /
+* @author fjw
+* @date 2024-05-16
+**/
+@Entity
+@Data
+@Table(name="sys_activity")
+public class SysActivity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "`activity_id`")
+    @ApiModelProperty(value = "活动编号")
+    private Integer activityId;
+
+    @Column(name = "`activity_name`",nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "活动名称")
+    private String activityName;
+
+    @Column(name = "`activity_progress`",nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "活动类型")
+    private String activityProgress;
+
+    @Column(name = "`create_by`",nullable = false)
+    @NotNull
+    @ApiModelProperty(value = "创建者")
+    private Integer createBy;
+
+    @Column(name = "`update_by`")
+    @ApiModelProperty(value = "更新者")
+    private Integer updateBy;
+
+    @Column(name = "`create_time`")
+    @CreationTimestamp
+    @ApiModelProperty(value = "创建时间")
+    private Timestamp createTime;
+
+    @Column(name = "`update_time`")
+    @UpdateTimestamp
+    @ApiModelProperty(value = "更新时间")
+    private Timestamp updateTime;
+
+    public void copy(SysActivity source){
+        BeanUtil.copyProperties(source,this, CopyOptions.create().setIgnoreNullValue(true));
+    }
+}
---
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/SysActivityServiceImpl.java
@@ -0,0 +1,110 @@
+/*
+*  Copyright 2019-2020 Zheng Jie
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+package me.zhengjie.modules.system.service.impl;
+
+import me.zhengjie.modules.system.domain.SysActivity;
+import me.zhengjie.utils.ValidationUtil;
+import me.zhengjie.utils.FileUtil;
+import lombok.RequiredArgsConstructor;
+import me.zhengjie.modules.system.repository.SysActivityRepository;
+import me.zhengjie.modules.system.service.SysActivityService;
+import me.zhengjie.modules.system.service.dto.SysActivityDto;
+import me.zhengjie.modules.system.service.dto.SysActivityQueryCriteria;
+import me.zhengjie.modules.system.service.mapstruct.SysActivityMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import me.zhengjie.utils.PageUtil;
+import me.zhengjie.utils.QueryHelp;
+import java.util.List;
+import java.util.Map;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import me.zhengjie.utils.PageResult;
+
+/**
+* @website https://eladmin.vip
+* @description 服务实现
+* @author fjw
+* @date 2024-05-16
+**/
+@Service
+@RequiredArgsConstructor
+public class SysActivityServiceImpl implements SysActivityService {
+
+    private final SysActivityRepository sysActivityRepository;
+    private final SysActivityMapper sysActivityMapper;
+
+    @Override
+    public PageResult<SysActivityDto> queryAll(SysActivityQueryCriteria criteria, Pageable pageable){
+        Page<SysActivity> page = sysActivityRepository.findAll((root, criteriaQuery, criteriaBuilder) -> QueryHelp.getPredicate(root,criteria,criteriaBuilder),pageable);
+        return PageUtil.toPage(page.map(sysActivityMapper::toDto));
+    }
+
+    @Override
+    public List<SysActivityDto> queryAll(SysActivityQueryCriteria criteria){
+        return sysActivityMapper.toDto(sysActivityRepository.findAll((root, criteriaQuery, criteriaBuilder) -> QueryHelp.getPredicate(root,criteria,criteriaBuilder)));
+    }
+
+    @Override
+    @Transactional
+    public SysActivityDto findById(Integer activityId) {
+        SysActivity sysActivity = sysActivityRepository.findById(activityId).orElseGet(SysActivity::new);
+        ValidationUtil.isNull(sysActivity.getActivityId(),"SysActivity","activityId",activityId);
+        return sysActivityMapper.toDto(sysActivity);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void create(SysActivity resources) {
+        sysActivityRepository.save(resources);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void update(SysActivity resources) {
+        SysActivity sysActivity = sysActivityRepository.findById(resources.getActivityId()).orElseGet(SysActivity::new);
+        ValidationUtil.isNull( sysActivity.getActivityId(),"SysActivity","id",resources.getActivityId());
+        sysActivity.copy(resources);
+        sysActivityRepository.save(sysActivity);
+    }
+
+    @Override
+    public void deleteAll(Integer[] ids) {
+        for (Integer activityId : ids) {
+            sysActivityRepository.deleteById(activityId);
+        }
+    }
+
+    @Override
+    public void download(List<SysActivityDto> all, HttpServletResponse response) throws IOException {
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (SysActivityDto sysActivity : all) {
+            Map<String,Object> map = new LinkedHashMap<>();
+            map.put("活动名称", sysActivity.getActivityName());
+            map.put("活动类型", sysActivity.getActivityProgress());
+            map.put("创建者", sysActivity.getCreateBy());
+            map.put("更新者", sysActivity.getUpdateBy());
+            map.put("创建时间", sysActivity.getCreateTime());
+            map.put("更新时间", sysActivity.getUpdateTime());
+            list.add(map);
+        }
+        FileUtil.downloadExcel(list, response);
+    }
+}
